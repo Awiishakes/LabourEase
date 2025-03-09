36d5130267b5c3d164923f5367d77ea36a34e0a0
Awais Shakoor <132345709+Awiishakes@users.noreply.github.com>
2025-03-09 16:17:12 +0500

    Update Home

--- Frontened/src/components/Loaders/HomeLoader.jsx
+++ Frontened/src/components/Loaders/HomeLoader.jsx
@@ -9,7 +9,7 @@ const HomeLoader = ({role, page}) => {
         {['client','visitor'].includes(role) &&
         <>
             <header className='w-full h-full bg-slate-200'>
-                <div className='h-[65dvh] min-[500px]:h-[75dvh] sm:h-[85dvh]'>
+                <div className='h-[65vh] min-[500px]:h-[75vh] sm:h-[85vh]'>
                     <svg className='absolute bottom-52 min-[500px]:bottom-32 sm:bottom-20 md:bottom-16' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                         <path fill="#fff" fillOpacity="1" d="M0,128L48,144C96,160,192,192,288,181.3C384,171,480,117,576,133.3C672,149,768,235,864,240C960,245,1056,171,1152,160C1248,149,1344,203,1392,229.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
                     </svg>
