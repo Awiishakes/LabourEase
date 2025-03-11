b1637daf3905d92b79ac4432de8b204714d9244d
Awais Shakoor <132345709+Awiishakes@users.noreply.github.com>
2025-03-11 16:51:40 +0500

    Update backenend

--- Backened/utills/jwtToken.js
+++ Backened/utills/jwtToken.js
@@ -7,16 +7,14 @@ export const sendToken = (user, statusCode, res, message) => {
             ),
             httpOnly: true,
             secure: true,
-            domain: "https://ingenious-perfection-production.up.railway.app",
-            sameSite: "none",
+            sameSite: "lax",
         },
         {
             expires: new Date(
                 Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
             ),
             secure: true,
-            domain: "https://ingenious-perfection-production.up.railway.app",
-            sameSite: "none",
+            sameSite: "lax",
         }
     ]
     res.status(statusCode).cookie('token', token, options[0]).cookie('checkToken', true, options[1]).json({
