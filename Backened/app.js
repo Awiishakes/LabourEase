73f576eaa961709dfd4982191286b7396af0d7c0
Awais Shakoor <132345709+Awiishakes@users.noreply.github.com>
2025-03-11 17:01:08 +0500

    Update frontend url

--- Backened/app.js
+++ Backened/app.js
@@ -15,7 +15,7 @@ dotenv.config({path: './config/config.env'})
 
 
 app.use(cors({
-    origin: 'https://labourease.pages.dev',
+    origin: 'https://labourease.netlify.app',
     method: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true
 }))
