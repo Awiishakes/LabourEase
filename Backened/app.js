30cc2b30424b72c7ef6e8f05ce05f6dcca399720
Awais Shakoor <132345709+Awiishakes@users.noreply.github.com>
2025-03-14 02:32:38 +0500

    Update frontened

--- Backened/app.js
+++ Backened/app.js
@@ -15,7 +15,7 @@ dotenv.config({path: './config/config.env'})
 
 
 app.use(cors({
-    origin: 'https://labourease.netlify.app',
+    origin: 'https://67d34e49594b11693a483c1e--labourease.netlify.app/login',
     method: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true
 }))
