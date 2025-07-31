545f9c03eb28f1a5272689dfa3a2a7318d3ab64a
Awiishakes <awaishakoor@gmail.com>
2025-07-31 21:03:51 +0500

    forth commit

--- Backened/utills/jwtToken.js
+++ Backened/utills/jwtToken.js
@@ -6,13 +6,15 @@ export const sendToken = (user, statusCode, res, message) => {
                 Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
             ),
             httpOnly: true,
-            secure: true
+            secure: true,
+            sameSite: 'None'
         },
         {
             expires: new Date(
                 Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
             ),
-            secure: true
+            secure: true,
+            sameSite: 'None'
         }
     ]
     res.status(statusCode).cookie('token', token, options[0]).cookie('checkToken', true, options[1]).json({
