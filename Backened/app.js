83227c632cb866ab10ad2044ba3b310d83c2ad33
Awiishakes <awaishakoor@gmail.com>
2025-08-01 18:36:24 +0500

    seventh commit

--- Backened/app.js
+++ Backened/app.js
@@ -15,7 +15,7 @@ dotenv.config({path: './config/config.env'})
 
 
 app.use(cors({
-    origin: ['https://labourease.netlify.app/'],
+    origin: 'https://labourease.netlify.app/',
     method: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true
 }))
